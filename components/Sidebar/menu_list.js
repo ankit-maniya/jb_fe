044ace9c06cfc93c2e813808488ff720dc072135
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-09-30 00:50:28 +0530

    Un workable routes disabled

--- components/Sidebar/menu_list.js
+++ components/Sidebar/menu_list.js
@@ -17,18 +17,18 @@ import {
 export const menuData = [
   { label: "Home", icon: IconHome, pagelink: "/home" },
   { label: "Dashboard", icon: IconDashboard, pagelink: "/dashboard" },
-  {
-    label: "Invoice",
-    icon: IconFileInvoice,
-    initiallyOpened: false,
-    child: ["/invoices", "/invoices/party_invoices"],
-    links: [
-      { label: "All Invoices", pagelink: "/invoices" },
-      { label: "party invoices", pagelink: "/invoices/party_invoices" },
-    ],
-  },
-  { label: "Balance Sheet", icon: IconWallet, pagelink: "/balance_sheets" },
-  { label: "Average", icon: IconPercentage, pagelink: "/averages" },
+  // {
+  //   label: "Invoice",
+  //   icon: IconFileInvoice,
+  //   initiallyOpened: false,
+  //   child: ["/invoices", "/invoices/party_invoices"],
+  //   links: [
+  //     { label: "All Invoices", pagelink: "/invoices" },
+  //     { label: "party invoices", pagelink: "/invoices/party_invoices" },
+  //   ],
+  // },
+  // { label: "Balance Sheet", icon: IconWallet, pagelink: "/balance_sheets" },
+  // { label: "Average", icon: IconPercentage, pagelink: "/averages" },
   {
     label: "Party",
     icon: IconUsers,
@@ -41,21 +41,21 @@ export const menuData = [
     ],
   },
   { label: "Cutting Types", icon: IconChecklist, pagelink: "/cutting_types" },
-  {
-    label: "Party Wise Details",
-    icon: IconNotes,
-    pagelink: "/loats/party_loats",
-  },
+  // {
+  //   label: "Party Wise Details",
+  //   icon: IconNotes,
+  //   pagelink: "/loats/party_loats",
+  // },
   { label: "Date Wise Details", icon: IconCalendar, pagelink: "/datewise/year" },
-  {
-    label: "Dustbin",
-    icon: IconTrash,
-    initiallyOpened: false,
-    child: ["/dustbin/dt_partys", "/dustbin/dt_cutting_types"],
-    links: [
-      { label: "Party", pagelink: "/dustbin/dt_partys" },
-      { label: "Cutting Type", pagelink: "/dustbin/dt_cutting_types" },
-    ],
-  },
+  // {
+  //   label: "Dustbin",
+  //   icon: IconTrash,
+  //   initiallyOpened: false,
+  //   child: ["/dustbin/dt_partys", "/dustbin/dt_cutting_types"],
+  //   links: [
+  //     { label: "Party", pagelink: "/dustbin/dt_partys" },
+  //     { label: "Cutting Type", pagelink: "/dustbin/dt_cutting_types" },
+  //   ],
+  // },
   { label: "Logout", icon: IconLogout },
 ];
