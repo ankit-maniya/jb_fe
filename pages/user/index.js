9f5c4d907e3d496825a95b76cecace1561dd2450
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-10-16 00:08:36 +0530

    Module Not found bug fixies

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUser } from "../../redux/reducers/userReducer";
+// import { fetchUser } from "../../redux/reducers/userReducer";
 
 const User = () => {
   const dispatch = useDispatch();
-  dispatch(fetchUser())
+  // dispatch(fetchUser())
   return (
     <div>
       hELLO
